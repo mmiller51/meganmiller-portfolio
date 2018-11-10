4d28296c0645207fbb2f0c77bf2f34064932fbf6
mmiller51 <43584585+mmiller51@users.noreply.github.com>
2018-11-09 20:36:56 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,33 +1,30 @@
 // JavaScript Document
 
-/*var modal = deocument.getElementById('modal01');
-
-var img = document.getElementById('myImg01');
-var modalImg = document.getElementByID("img01");
-var captionText = document.getElementById("caption01");
-img.onclick = function(){
-	modal.style.display = "block";
-	modalIMG.src = this.src;
-	captionText.innerHTML = this.alt;
-}
-
-var span = document.getElementByClassName("close")[0];
-
-span.onclick = function() {
-	modal.style.display = "none";
-}*/
 
 window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
+/*function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-100px";
   }
-}
+}*/
 
 
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("myButton").style.display = "block";
+	} else {
+		document.getElementById("myButton").style.display = "none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 }
